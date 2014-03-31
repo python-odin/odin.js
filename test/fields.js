0f57f6c035cc80c18ded348e29d9118f89c4bcd8
Tim Savage <tim.savage@poweredbypenguins.org>
2014-03-31 23:55:52 +1100

    Added some unit tests added coverage report

--- test/fields.js
+++ test/fields.js
@@ -1,3 +1,37 @@
 (function () {
+  test("hasDefault", function () {
+    var field = new Odin.Field({defaultValue: 123});
 
+    ok(field.hasDefault());
+    equal(field.getDefault(), 123);
+  });
+
+  test("noDefault", function () {
+    var field = new Odin.Field();
+
+    ok(!field.hasDefault());
+    equal(field.getDefault(), null);
+  });
+
+  test("setAttributesFromName", function () {
+    var field = new Odin.Field();
+
+    equal(field.name, null);
+    field.setAttributesFromName('a_name');
+    equal(field.name, 'a_name');
+    equal(field.attname, 'a_name');
+    equal(field.verboseName, 'a name');
+    equal(field.verboseNamePlural, 'a names');
+  });
+
+  test("setAttributesFromName", function () {
+    var field = new Odin.Field();
+
+    equal(field.name, null);
+    field.setAttributesFromName('a_name');
+    equal(field.name, 'a_name');
+    equal(field.attname, 'a_name');
+    equal(field.verboseName, 'a name');
+    equal(field.verboseNamePlural, 'a names');
+  });
 }());
