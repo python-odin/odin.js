9f9c74ebe7834c91062bd6f8a17e33777707e871
Tim Savage <tim.savage@poweredbypenguins.org>
2014-04-01 00:15:24 +1100

    Test cases for helpers

---
+++ test/helpers.js
@@ -0,0 +1,20 @@
+(function () {
+  test('isEmpty', function () {
+    ok(Odin.isEmpty(null));
+    ok(Odin.isEmpty(undefined));
+    ok(Odin.isEmpty(''));
+    ok(!Odin.isEmpty('abc'));
+    ok(!Odin.isEmpty(123));
+  });
+
+  test('setDefault', function () {
+    var data = {foo: 'bar'};
+
+    equal(data.foo, 'bar');
+    equal(Odin.setDefault(data, 'foo'), 'bar');
+    equal(Odin.setDefault(data, 'foo', 'eek'), 'bar');
+    equal(Odin.setDefault(data, 'eek'), null);
+    equal(Odin.setDefault(data, 'abc', 'def'), 'def');
+    deepEqual(data, {foo: 'bar', 'eek': null, 'abc': 'def'})
+  });
+}());
